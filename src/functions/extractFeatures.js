aaad63ec7e2ef7ac1114f49919d16ba579106bb5
Bob Wei <bob.wei@linecorp.com>
2018-08-10 17:45:32 +0800

    Add serialize option

--- src/functions/extractFeatures.js
+++ src/functions/extractFeatures.js
@@ -3,15 +3,19 @@ import R from 'ramda';
 import mapAsync from 'src/utils/mapAsync';
 
 import detectFaces from './detectFaces';
+import serializeFeature from './serializeFeature';
 
 const fn = (dependencies, data) => {
-  const { arcface } = dependencies;
+  const { arcface, serialize = false } = dependencies;
   return Promise.resolve(data)
     .then(detectFaces(dependencies))
     .then(
       mapAsync(
-        mapAsync(({ asvl, rcFace, lfaceOrient }) =>
-          arcface.extractFeature(asvl, rcFace, lfaceOrient),
+        R.pipeP(
+          mapAsync(({ asvl, rcFace, lfaceOrient }) =>
+            arcface.extractFeature(asvl, rcFace, lfaceOrient),
+          ),
+          R.when(R.always(serialize), R.map(serializeFeature)),
         ),
       ),
     );
